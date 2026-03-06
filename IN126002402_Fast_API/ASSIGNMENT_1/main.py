785ff46d40a528f2eeabb969ec71b9d573424928
techdhanu <25dhanush03@gmail.com>
2026-03-06 18:17:52 +0530

    Task 1 fastapi committed

---
+++ IN126002402_Fast_API/ASSIGNMENT_1/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+products = [
+    {"id": 1, "name": "Wireless Mouse", "price": 599, "category": "Electronics", "in_stock": True},
+    {"id": 2, "name": "Notebook", "price": 99, "category": "Stationery", "in_stock": True},
+    {"id": 3, "name": "Pen Set", "price": 49, "category": "Stationery", "in_stock": True},
+    {"id": 4, "name": "USB Cable", "price": 199, "category": "Electronics", "in_stock": False},
+    {"id": 5, "name": "Laptop Stand", "price": 1299, "category": "Electronics", "in_stock": True},
+    {"id": 6, "name": "Mechanical Keyboard", "price": 2499, "category": "Electronics", "in_stock": True},
+    {"id": 7, "name": "Webcam", "price": 1899, "category": "Electronics", "in_stock": False}
+]
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI is working!"}
+
+@app.get("/products")
+def get_products():
+    return {
+        "products": products,
+        "total": len(products)
+    }
+@app.get("/products/category/{category_name}")
+def get_by_category(category_name: str):
+    result = [p for p in products if p["category"] == category_name]
+
+    if not result:
+        return {"error": "No products found in this category"}
+
+    return {
+        "category": category_name,
+        "products": result,
+        "total": len(result)
+    }
